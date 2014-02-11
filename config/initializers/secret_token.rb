ee45a64b36480471503e658b7d644a0a3cebde83
Justin Wu <justin@roxollo.com>
2014-02-10 21:08:38 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Breakfit::Application.config.secret_key_base = '235a71558e0d42029807e7cafac3c683b8b50586c601025c83a52278302c2dbaa2033efc062640ef5eb14ff35407a1b96cf6fc09d2b8b932ec98da6b0717986b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Breakfit::Application.config.session_store :cookie_store, key: '_breakfit_session'
