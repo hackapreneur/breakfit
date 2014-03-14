ef2d6506b41cdb2d60edb6af181b02cee2ce82f4
Justin Wu <justin@roxollo.com>
2014-03-13 18:48:14 -0700

    Push

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Breakfit::Application.routes.draw do
 
   root "pages#home"
+  get 'confirmation' => 'pages#confirmation'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
